4144ab420d85066971076615bed82354d8e7082b
Laraning <bruno.falcao@laraning.com>
2018-08-05 23:58:49 +0200

    File changes

--- src/helpers.php
+++ src/helpers.php
@@ -13,3 +13,37 @@ if (!function_exists('d')) {
         }
     }
 }
+
+if (!function_exists('get_uri_collection')) {
+    /**
+     * Gets the uri collection (segments).
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    function get_uri_collection()
+    {
+        $router = app()->make('router');
+        $segments = collect(explode('/', $router->getCurrentRoute()->uri));
+
+        return $segments;
+    }
+}
+
+if (!function_exists('get_uri_collection_without_bindings')) {
+    /**
+     * Gets the uri collection without bindings.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    function get_uri_collection_without_bindings()
+    {
+        $uri = get_uri_collection();
+
+        // Remove { .. } items.
+        $segments = $uri->filter(function ($value, $key) {
+            return $value[0] != '{';
+        });
+
+        return $segments;
+    }
+}
