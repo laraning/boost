76fbb9da177631841b795eb325321f5ca111c57d
Laraning <bruno.falcao@laraning.com>
2018-08-05 22:15:39 +0000

    Apply fixes from StyleCI

--- src/Providers/BaseServiceProvider.php
+++ src/Providers/BaseServiceProvider.php
@@ -57,6 +57,7 @@ class BaseServiceProvider extends ServiceProvider
 
         Blade::directive('pushonce', function ($expression) {
             $var = '$__env->{"__pushonce_" . md5(__FILE__ . ":" . __LINE__)}';
+
             return "<?php if(!isset({$var})): {$var} = true; \$__env->startPush({$expression}); ?>";
         });
 
